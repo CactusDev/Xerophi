8b30a284d15eb9009f46eab744e930fd475aa607
Innectic <innectic@gmail.com>
2017-10-22 08:43:34 -0700

    Quote data validation, cleanup controller

--- src/endpoints/quote/controller.ts
+++ src/endpoints/quote/controller.ts
@@ -3,8 +3,9 @@ import * as Hapi from "hapi";
 import * as Boom from "boom";
 
 import { MongoHandler } from "../../mongo";
+import { theTime } from "../../util";
 
-const moment = require("moment-strftime");
+const messagePacketKeys = ["type", "data"];
 
 export class QuoteController {
 
@@ -12,14 +13,22 @@ export class QuoteController {
 
 	}
 
-	private async verifyData(data: string): Promise<boolean> {
-		return true; // TODO: How does this even get verified?
+	private async verifyData(data: any[]): Promise<boolean> {
+		for (let packet of data) {
+			for (let key of messagePacketKeys) {
+				if (!packet[key]) return false;
+			}
+		}
+		return data.length >= 1;
 	}
 
 	public async getQuote(request: Hapi.Request, reply: Hapi.ReplyNoContinue) {
 		const id = +request.params["id"];
 		const channel = request.params["channel"];
 		const random = !!request.query && !!request.query.random || false;
+		if (random) {
+			return await this.getRandomQuote(request, reply);
+		}
 
 		if (id) {
 			const response = await this.mongo.getQuote(channel, false, id);
@@ -31,31 +40,32 @@ export class QuoteController {
 				return reply(response);
 			}
 			return reply(Boom.notFound("Invalid quote"));
-		} else {
-			if (random) {
-				const response = await this.mongo.getQuote(channel, true);
-				if (!response || response.deletedAt) {
-					return reply(Boom.notFound("Invalid quote"));
+		}
+		const response = await this.mongo.getAllQuotes(channel);
+		for (let quote of response) {
+			if (quote.deletedAt) {
+				const index = response.indexOf(quote);
+				if (index !== -1) {
+					response.splice(index, 1);
 				}
-				delete response.deletedAt;
-				if (!response.deletedAt && response.enabled) {
-					return reply(response);
-				}
-				return reply(Boom.internal("Got invalid quote from filter?"));
+			} else {
+				delete quote.deletedAt;					
 			}
- 			const response = await this.mongo.getAllQuotes(channel);
-			for (let quote of response) {
-				if (quote.deletedAt) {
-					const index = response.indexOf(quote);
-					if (index !== -1) {
-						response.splice(index, 1);
-					}
-				} else {
-					delete quote.deletedAt;					
-				}
-			}
+		}
+		return reply(response);
+	}
+
+	private async getRandomQuote(request: Hapi.Request, reply: Hapi.ReplyNoContinue) {
+		const channel = request.params["channel"];
+		const response = await this.mongo.getQuote(channel, true);
+		if (!response || response.deletedAt) {
+			return reply(Boom.notFound("Invalid quote"));
+		}
+		delete response.deletedAt;
+		if (!response.deletedAt && response.enabled) {
 			return reply(response);
 		}
+		return reply(Boom.internal("Got invalid quote from filter?"));
 	}
 
 	public async createQuote(request: Hapi.Request, reply: Hapi.ReplyNoContinue) {
@@ -71,7 +81,7 @@ export class QuoteController {
 			quoteId: -1,
 			channel: channel,
 			quoted: quoted,
-			createdAt: moment().strftime("%a %b %d %H:%M:%S %Y"),
+			createdAt: theTime(),
 			deletedAt: null,
 			enabled: true,
 			count: 0,
@@ -82,7 +92,7 @@ export class QuoteController {
 		delete quote.deletedAt;
 		return reply({
 			created: true,
-			name
+			quotedId: quote.quoteId
 		}).code(201);
 	}
 
