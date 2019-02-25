71e394121bb85906e5243f44735d1a76791f59c7
Innectic <innectic@gmail.com>
2019-02-25 01:06:18 -0800

    Start conversion to new response system

--- src/endpoints/mod.rs
+++ src/endpoints/mod.rs
@@ -4,7 +4,9 @@ pub mod state;
 pub mod authorization;
 pub mod quote;
 
+use rocket::{Response, http::{Status, ContentType}};
 use rocket_contrib::json::JsonValue;
+use std::io::Cursor;
 
 pub fn generate_error(code: u32, message: Option<String>) -> JsonValue {
 	json!({
@@ -13,4 +15,15 @@ pub fn generate_error(code: u32, message: Option<String>) -> JsonValue {
 			"message": message.unwrap_or("".to_string())
 		})
 	})
+}
+
+pub fn generate_response<'r>(code: Status, json: JsonValue) -> Response<'r> {
+	let mut response = Response::build();
+	let json = json.to_string();
+
+    response.status(code);
+	response.sized_body(Cursor::new(json));
+	response.header(ContentType::JSON);
+
+	response.finalize()
 }
--- src/endpoints/quote.rs
+++ src/endpoints/quote.rs
@@ -1,9 +1,14 @@
 
-use rocket_contrib::json::{JsonValue, Json};
+use rocket_contrib::json::Json;
 
-use rocket::State;
+use rocket::{
+	State,
+	Response,
+	http::Status
+};
+
 use crate::{
-	DbConn, endpoints::generate_error,
+	DbConn, endpoints::{generate_error, generate_response},
 	database::{
 		handler::HandlerError,
 		structures::Message
@@ -16,71 +21,71 @@ pub struct PostQuote {
 }
 
 #[get("/<channel>")]
-pub fn get_quote(handler: State<DbConn>, channel: String) -> JsonValue {
+pub fn get_quote<'r>(handler: State<DbConn>, channel: String) -> Response<'r> {
 	let quotes = handler.lock().expect("db lock").get_quote(&channel, None);
 	match quotes {
-		Ok(quotes) => json!({ "data": quotes }),
-		Err(HandlerError::Error(_)) => json!({ "data": json!([]) }),
+		Ok(quotes) => generate_response(Status::Ok, json!({ "data": quotes })),
+		Err(HandlerError::Error(_)) => generate_response(Status::NotFound, json!({ "data": json!([]) })),
 		Err(e) => {
 			println!("Internal error getting quote: {:?}", e);
-			generate_error(500, None)
+			generate_response(Status::InternalServerError, generate_error(500, None))
 		}
 	}
 }
 
 #[get("/<channel>/<id>", rank = 3)]
-pub fn get_quote_by_id(handler: State<DbConn>, channel: String, id: u32) -> JsonValue {
+pub fn get_quote_by_id<'r>(handler: State<DbConn>, channel: String, id: u32) -> Response<'r> {
 	let quote = handler.lock().expect("db lock").get_quote(&channel, Some(id));
 	match quote {
-		Ok(quote) => json!({ "data": quote[0] }),
-		Err(HandlerError::Error(_)) => json!({ "data": json!({}) }),
+		Ok(quote) => generate_response(Status::Ok, json!({ "data": quote[0] })),
+		Err(HandlerError::Error(_)) => generate_response(Status::NotFound, json!({ "data": json!([]) })),
 		Err(e) => {
 			println!("Internal error getting quote: {:?}", e);
-			generate_error(500, None)
+			generate_response(Status::InternalServerError, generate_error(500, None))
 		}
 	}
 }
 
 #[post("/<channel>/create", rank = 2, format = "json", data = "<quote>")]
-pub fn create_quote(handler: State<DbConn>, channel: String, quote: Json<PostQuote>) -> JsonValue {
+pub fn create_quote<'r>(handler: State<DbConn>, channel: String, quote: Json<PostQuote>) -> Response<'r> {
 	let result = handler.lock().expect("db lock").create_quote(&channel, quote.into_inner());
 	match result {
-		Ok(id) => json!({
+		Ok(id) => generate_response(Status::Created, json!({
 			"data": json!({
 				"created": true,
 				"id": id
 			})
-		}),
-		Err(HandlerError::Error(e)) => generate_error(401, Some(e)),
+		})),
+		Err(HandlerError::Error(_)) => generate_response(Status::BadRequest, json!({ "data": json!([]) })),
 		Err(e) => {
 			println!("Internal error creating quote: {:?}", e);
-			generate_error(500, None)
+			generate_response(Status::InternalServerError, generate_error(500, None))
 		}
 	}
 }
 
 #[get("/<channel>/random", rank = 1)]
-pub fn get_random_quote(handler: State<DbConn>, channel: String) -> JsonValue {
+pub fn get_random_quote<'r>(handler: State<DbConn>, channel: String) -> Response<'r> {
 	let quote = handler.lock().expect("db lock").get_random_quote(&channel);
 	match quote {
-		Ok(quote) => json!({ "data": quote }),
-		Err(HandlerError::Error(_)) => json!({ "data": json!({}) }),
+		Ok(quote) => generate_response(Status::Ok, json!({ "data": quote })),
+		Err(HandlerError::Error(_)) => generate_response(Status::NotFound, json!({ "data": json!([]) })),
 		Err(e) => {
 			println!("Internal error getting quote: {:?}", e);
-			generate_error(500, None)
+			generate_response(Status::InternalServerError, generate_error(500, None))
 		}
 	}
 }
 
 #[delete("/<channel>/<id>", rank = 4)]
-pub fn delete_quote(handler: State<DbConn>, channel: String, id: u32) -> JsonValue {
+pub fn delete_quote<'r>(handler: State<DbConn>, channel: String, id: u32) -> Response<'r> {
 	let result = handler.lock().expect("db lock").delete_quote(&channel, id);
 	match result {
-		Ok(()) => json!({ "data": json!({ "deleted": true }) }),
-		Err(HandlerError::Error(_)) => json!({ "data": json!({}) }),
+		Ok(()) => generate_response(Status::NoContent, json!({})),
+		Err(HandlerError::Error(_)) => generate_response(Status::NotFound, json!({"data": {}})),
 		Err(e) => {
 			println!("Internal error getting quote: {:?}", e);
-			generate_error(500, None)
+			generate_response(Status::InternalServerError, generate_error(500, None))
 		}
 	}
 }
